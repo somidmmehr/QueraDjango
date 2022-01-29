ec4b62e272722a2add8e0161312f52cf669338d2
S.OMID M.MEHR <s.omid.m.mehr@gmail.com>
2022-01-29 11:11:06 +0330

    Commit to revert

--- first_project/first_projrct/urls.py
+++ first_project/first_projrct/urls.py
@@ -1,4 +1,4 @@
-"""first_projrct URL Configuration
+"""first_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/

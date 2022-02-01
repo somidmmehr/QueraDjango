885e2e3bbf667d180c440d2ce7dc1b554cfaec51
S.OMID M.MEHR <s.omid.m.mehr@gmail.com>
2022-02-01 13:16:00 +0330

    Created first_app model "Person" and it's migrations

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=150)
+    username = models.CharField(max_length=150)
+    last_login = models.DateTimeField(auto_now_add=True, null=True)
+    is_super = models.BooleanField(default=False)
+    email = models.EmailField(max_length=250, null=True)
+
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from first_app.views import signup_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', signup_view),
 ]

6c078e97792aa47d2f2eed6f4b29fe3d7a850755
S.OMID M.MEHR <s.omid.m.mehr@gmail.com>
2022-02-23 06:21:18 +0330

    some view practices

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('signup/<str:name>/', signup_view),
+    path('lasto/', last_online),
+    path('book/', show_book),
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,7 +1,24 @@
+from .models import *
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 def signup_view(request, name):
     return HttpResponse(f'Signup Completed! {name}')
+
+
+def last_online(request):
+    count = int(count) if (count := request.GET.get("count", 10)) != '' else 1
+    people = Person.people.latest_logins(count).values_list('name', flat=True)
+    splitter = '<br/>'
+    response = splitter.join(map(str, people))
+    return HttpResponse(response)
+
+
+@csrf_exempt
+def show_book(request):
+    book_id = request.POST.get('book_id') or 2
+    book = get_object_or_404(Book.objects.using("MySQL"), id=book_id)
+    return HttpResponse(f'کتاب {book.name}')
